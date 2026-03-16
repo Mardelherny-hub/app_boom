5eb519df46987f4359780fb329bb47e1052cd988
Mardelherny <alcaldevictor1@gmail.com>
2026-03-16 20:54:34 -0300

    feat: correcciones en portfolio

--- resources/views/frontend/portfolio/index.blade.php
+++ resources/views/frontend/portfolio/index.blade.php
@@ -14,7 +14,7 @@
                 <div>
                     <img src="{{ asset('images/portfolio/portfolio_header.webp') }}" 
                         alt="Portfolio Boom Studio" 
-                        class="w-full rounded-lg">
+                        class="w-full rounded-lg mix-blend-multiply">
                 </div>
                 
                 {{-- Texto DERECHA --}}
